46d9f22b3b668d5ee3a54bd093c834e704835729
2522500059-source <2522500059.mahasiswa.atmaluhur.ac.id>
2025-10-29 21:56:27 +0700

    update pertamuan-05

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -11,4 +11,30 @@ document.getElementById("menuToggle").addEventListener("click", function() {
     [nama, email, pesan].forEach(el => el.style.border = "");
 
     let isValid = true;
+
+    if (nama.value.trim().length < 3) {
+        showError(nama, "Nama minimal 3 huruf dan tidak boleh kosong");
+       isValid = false; 
+    } else if (!/^[A-Za-z\s]+$/.test(nama.value)) {
+        showError(nama, "Nama hanya boleh berisi huruf dan spasi.");
+        isValid = false;
+    }
+
+    if (email.value.trim() === "") {
+        showError(email, "Email wajib diisi.");
+        isValid = false;
+    } else if (!/^[^\s@+@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+        showError(email, "Format email tidak valid. Contoh: nama@gmail.com");
+        isvalid = false;
+    }
+
+    if (pesan.value.trim().length < 10) {
+        showError(pesan, "Pesan minimal 10 karakter agar lebih jelas.");
+        isValid = false;
+    }
+    if(!isvalid) {
+        e.preventDefault();
+    } else {
+        alert("Terima kasih," + nama.value + "!\nPesan Anda telah dikirim.");
+    }
 });
