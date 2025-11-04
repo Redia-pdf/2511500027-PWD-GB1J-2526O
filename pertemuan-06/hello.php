fe0a9e667633d10abdc10c28feff71e3ad62965e
2522500059-source <2522500059.mahasiswa.atmaluhur.ac.id>
2025-11-04 20:15:18 +0700

    update pertamuan-06

--- pertemuan-06/hello.php
+++ pertemuan-06/hello.php
@@ -79,4 +79,15 @@ echo "\$a == $b : "; var_dump($a == $b);
 echo "\$a === \$b : "; var_dump($a === $b);
 echo "\$c == \$d : "; var_dump($c == $d);
 echo "\$c === \$d : "; var_dump($c === $d);
+
+$nilai = 80;
+
+if ($nilai >= 90) {
+    echo "A";
+} elseif ($nilai >= 80) {
+    echo "B";
+}else {
+    echo "C";
+}
+
 ?>
