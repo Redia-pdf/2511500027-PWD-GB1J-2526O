6d1d2d76dae1a86cf530aaaff7dc56a979b285e9
2522500059-source <2522500059.mahasiswa.atmaluhur.ac.id>
2025-11-04 19:41:46 +0700

    update pertamuan-06

--- pertemuan-06/hello.php
+++ pertemuan-06/hello.php
@@ -14,4 +14,40 @@
     var_dump($umur);
     var_dump($tinggi);
     var_dump($aktif);
+
+    $nama = "Redia Yuana Dinandika Tjandring";
+    $umur = 18;
+    $tinggi = 1.5;
+    $aktif = true;
+    $hobi = ["Sepak Bola", "Dance", "Baca Komik"];
+    $mahasiswa = (object)[
+        "nim" => "2511500027",
+        "nama" => "Redia Yuana Dinandika Tjandring",
+        "prodi" => "Teknik Informatika"
+    ];
+  $nilai_akhir = NULL;
+  echo "<h2> Demo Tipe Data PHP</h2>";
+  
+  echo "<pre>";
+  echo "string:\n";
+  var_dump($nama);
+
+  echo "\nInteger:\n";
+  var_dump($umur);
+
+  echo "\nFloat:\n";
+  var_dump($tinggi);
+
+  echo "\nBoolean:\n";
+  var_dump($aktif);
+
+  echo "\nArray\n";
+  var_dump($hobi);
+
+  echo "\nObject:\n";
+  var_dump($mahasiswa);
+
+  echo "\nNULL\n";
+  var_dump($nilai_akhir);
+  echo "</pre>";
 ?>
