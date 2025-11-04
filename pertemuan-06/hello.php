5821be31101a39848f5cf8208459b73ea7903e27
2522500059-source <2522500059.mahasiswa.atmaluhur.ac.id>
2025-11-04 19:11:07 +0700

    update pertamuan-06

--- pertemuan-06/hello.php
+++ pertemuan-06/hello.php
@@ -1,3 +1,17 @@
 <?php
     echo "Halo Dunia!";
+
+    $nama = "Redia";
+    $umur = "18";
+    $tinggi = "1.5";
+    $aktif = "true";
+
+    echo "nama: $nama <br>";
+    echo "umur: $umur <br>";
+    echo "tinggi: $tinggi meter <br>";
+    echo "status aktif: " . ($aktif ? "Ya" : "Tidak") . "<br>";
+    var_dump($nama);
+    var_dump($umur);
+    var_dump($tinggi);
+    var_dump($aktif);
 ?>
