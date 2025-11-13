24931c07ee3c5b7cfc5ab9a82050fd6d1dc7e7e0
2522500059-source <2522500059.mahasiswa.atmaluhur.ac.id>
2025-11-13 14:45:30 +0700

    sesuaikan get ke post

--- pertemuan-07/post.php
+++ pertemuan-07/post.php
@@ -69,7 +69,7 @@ session_start();
 
     <section id="contact">
       <h2>Kontak Kami</h2>
-      <form action="proses.php"method="GET">
+      <form action="post_proses.php"method="POST">
 
         <label for="txtNama"><span>Nama:</span>
           <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
