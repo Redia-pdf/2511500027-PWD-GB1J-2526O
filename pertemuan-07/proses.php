d7e5bfdd8cc288dcd8463599d010b94d510178a2
2522500059-source <2522500059.mahasiswa.atmaluhur.ac.id>
2025-11-13 11:53:05 +0700

    nuat kondisi kehadiran dibawah 70 = grade E

--- pertemuan-07/proses.php
+++ pertemuan-07/proses.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
-  $sesnama =  $_POST["txtNama"];
-  $sesemail = $_POST["txtEmail"];
-  $sespesan = $_POST["txtPesan"];
   $_SESSION ["sesnama"] = $sesnama;
   $_SESSION ["sesemail"] = $sesemail;
   $_SESSION ["sespesan"] = $sespesan;
   header("Location : index.php");
-  exit();
 ?>
