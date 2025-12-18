7841646eeddf5e6d7b56c16a41ef11a1213c91d1
2522500059-source <2522500059.mahasiswa.atmaluhur.ac.id>
2025-12-18 17:45:05 +0700

    Tambahkan penampil flash sukses maupun flash error menggunakan session pada read.php

--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -1,9 +1,35 @@
 <?php
 require 'koneksi.php';
+require 'fungsi.php';
 
 $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
 $q = mysqli_query($conn, $sql);
+if (!$q) {
+    die("query error: " . mysqli_error($conn));
+}
 ?>
+
+<?php
+    $flash_sukses = $_SESSION['flash_sukses'] ?? ''; #jika query sukses
+    $flash_error  = $_SESSION['flash_error'] ?? ''; #jika ada error
+    #bersihkan  session ini
+    unset($_SESSION['flash_sukses'], $_SESSION['flash_error']);
+?>
+
+<?php if(!empty($flash_sukses)): ?>
+    <div style="padding:10px; margin-bottom:10px;
+    background:#d4edda; color:#155724; border-radius:6px;">
+    <?= $flash_sukses; ?>
+    </div>
+<?php endif; ?>
+
+<?php if(!empty($flash_error)): ?>
+    <div style="padding:10px; margin-bottom:10px;
+    background:#f8d7da; color:#721c24; border-radius:6px;">
+    <?= $flash_error; ?>
+    </div>
+<?php endif; ?>
+
 <table border="1" cellpadding="8" cellspacing="0">
     <tr>
         <th>NO</th>
@@ -24,7 +50,7 @@ $q = mysqli_query($conn, $sql);
             <td> <?= htmlspecialchars($row['cnama']); ?></td>
             <td> <?= htmlspecialchars($row['cemail']); ?></td>
             <td> <?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
-            <td><?= date('d-m-Y H:i', strtotime($row['created_at'])); ?></td>
+            <td> <? formatTanggal(htmlspecialchars($row['dcreated_at'])); ?></td>
         </tr>
     <?php endwhile; ?>
 </table>
